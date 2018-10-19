b801460e8e722a83f4bd09efd0c29b997ea81f86
Damiano Raab <DamianoRaab11@gmail.com>
2018-10-19 13:45:07 -0400

    new file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -123,5 +123,7 @@ $(document).ready(function () {
             renderCharacters(currSelectedCharacter, "#selectedCharacter");
             renderCharacters(combatants, "#enemyCharacter");
         }
-    })	
+    });
+    
+    
 });
