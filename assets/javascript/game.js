69f8d33c6738712cb744ce2c5ced17452c7a2847
Damiano Raab <DamianoRaab11@gmail.com>
2018-10-19 13:37:23 -0400

    new file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,41 +38,71 @@ $(document).ready(function () {
     
     };
     var currSelectedCharacter;
-    var combatants = [];
+    var combatants = []
+    var currDefender;
     
 
     // console.log(characters);
 
     // This function will render fighters to the page
-    var renderOne = function(character, renderArea) {
+    var renderOne = function(character, renderArea, charStatus) {
         var charDiv = $("<div class = 'character' data-name = '" + character.name + "'>");
         var charName = $("<div class = 'character-name'>").text(character.name);
         var charImage = $("<img alt = 'image' class = 'character-image'>").attr("src",character.imageUrl);
         var charHealth = $("<div class = 'character-health'>").text(character.health);
         charDiv.append(charName).append(charImage).append(charHealth);
         $(renderArea).append(charDiv);
-    }
+
+        if(charStatus === "enemy") {
+            $(charDiv).addClass("enemy");
+        }
+        else if (charStatus === "defender") {
+            currDefender = character;
+            $(charDiv).addClass("targetEnemy");
+        }
+    };
+
 
     var renderCharacters = function(charObj, areaRender) {
         if (areaRender === "#characterSection") {
             $(areaRender).empty();
             for (var key in charObj) {
                 if(charObj.hasOwnProperty(key)) {
-                    renderOne(charObj[key], areaRender);
+                    renderOne(charObj[key], areaRender, "");
                 }
             }
              
         }
         if (areaRender === "#selectedCharacter") {
-            renderOne(charObj, areaRender);
+            renderOne(charObj, areaRender, "");
         }
         if (areaRender === "#enemyCharacter") {
             for(var i = 0; i < charObj.length; i++) {
-                renderOne(charObj[i], areaRender);
+                renderOne(charObj[i], areaRender, "enemy");
+            }
+        
+            //Create on.click event for each enemy
+            $(document).on("click", ".enemy", function(){
+                var name = ($(this).attr("data-name"));
+
+                //if there is no defender, the clicked enemy will become defender
+                if($("#defender").children().length === 0) {
+                    renderCharacters(name, "#defender");
+                    $(this).hide();
+                }
+            });
+        
+        }
+        if (areaRender === "#defender") {
+            $(areaRender).empty();
+            for( var i = 0; i < combatants.length; i++) {
+                if(combatants[i].name === charObj) {
+                    renderOne(combatants[i], areaRender, "defender");
+                }
             }
         }
 
-    }
+    };
     renderCharacters(characters, "#characterSection");
 
     $(document).on("click", ".character", function(){
